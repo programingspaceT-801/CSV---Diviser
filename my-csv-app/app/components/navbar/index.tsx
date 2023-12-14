d4f82ddef221ddd500c195c9e71450c5a78f5fee
Marcos Alves <marcosalvesjr007@gmail.com>
2023-12-14 16:22:09 +0000

    feat: removeul deploy

--- my-csv-app/app/components/navbar/index.tsx
+++ my-csv-app/app/components/navbar/index.tsx
@@ -8,28 +8,27 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className="w-full flex py-2 px-6 justify-between items-center navbar bg-white">
-      <img src='/logo (1) (1).png' alt="hoobank" className="w-[70px] h-[auto]" />
+    <nav className="w-full flex py-2 px-8 justify-between items-center navbar bg-trasnparent">
+      <img src='/logo (1) (1).png' alt="hoobank" className="w-[50px] h-[auto]" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-     
+        <li className="d-flex justify-between w-100">
+          <a className="mr-6 cursor-pointer">Home</a>
+          <a className="mr-6 cursor-pointer">PHtech</a>
+        </li>
       </ul>
 
       <div className="sm:hidden flex flex-1 justify-end items-center">
-        <img
-          src={toggle ? iconsclose : iconsmenu}
-          alt="menu"
-          className="w-[28px] h-[28px] object-contain"
-          onClick={() => setToggle(!toggle)}
-        />
 
         <div
-          className={`${
-            !toggle ? "hidden" : "flex"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-8 min-w-[140px] rounded-xl sidebar`}
+          className={`${!toggle ? "hidden" : "flex"
+            } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-8 min-w-[140px] rounded-xl sidebar`}
         >
-          <ul className="list-none flex justify-end items-start flex-1 flex-col">
-          
+          <ul className="list-none flex justify-end items-start flex-1 flex-col text-white">
+            <li>
+              <a>Home</a>
+              <a>PHtech</a>
+            </li>
           </ul>
         </div>
       </div>
